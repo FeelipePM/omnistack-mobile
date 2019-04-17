8ab7ea7ce5257ea5d1c460fed848231015000f92
Felipe Machado <feelipe@email.com>
2019-04-16 23:09:47 -0300

    criado o repositório da parte mobile em react-native

---
+++ src/pages/Main/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+import logo from "../../assets/logo.png";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Crie um Box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+        />
+
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Criar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
